e433ba1193f1e08efb8a8138d717a69e021844fe
Joseph Quinn <quinnjr@users.noreply.github.com>
2019-02-01 18:34:10 -0500

    Update destiny2-lfg-reports.user.js

--- destiny2-lfg-reports.user.js
+++ destiny2-lfg-reports.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Destiny 2 LFG Reports
 // @namespace    http://bungie.net/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Appends a raid report link and pvp info link to public fireteams on the Destiny LFG app.
 // @author       Joseph R. Quinn <quinn.josephr@protonmail.com>
 // @copyright    2019 Joseph R. Quinn
@@ -69,7 +69,7 @@
         break;
     }
     
-    return pvp_url.concat(platform, '/', user.innerText, '/detailed', (platform === 'pc') ? "".concat('?mbmid=', getPCId(user)) : null);
+    return pvp_url.concat(platform, '/', user.innerText, '/detailed', (platform === 'pc') ? "".concat('?mbmid=', getPCId(user)) : '');
     // return 'javascript:alert("PVP Report for PC players is currently in development");';
   }
 
